cf9ab737f81a3def6355815f7d2fa37baf8884dc
Biagio <biazzo90@libero.it>
2014-02-17 10:58:43 +0100

    FirstLevel add button to change script
    FirstLevel add button to change script, delete letter

---
+++ Assets/ButtonListener.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonListener : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			disableAllScript (player);
+			player.GetComponent <DiscreteMouseController>().enabled  	= true;
+		} else if (Input.GetKey (KeyCode.K)) {
+			disableAllScript (player);
+			player.GetComponent <KinematicMouseController>().enabled  	= true;
+		} else if (Input.GetKey (KeyCode.A)) {
+			disableAllScript (player);
+			player.GetComponent <DifferentialMouseController>().enabled = true;
+		} else if (Input.GetKey (KeyCode.F)) {
+			disableAllScript (player);
+			player.GetComponent <DynamicMouseController>().enabled  	= true;
+		} else if (Input.GetKey (KeyCode.C)) {
+			disableAllScript (player);
+			player.GetComponent <CarMouseController>().enabled  		= true;
+		}
+	}
+
+	private void disableAllScript(GameObject player) {
+		MonoBehaviour[] scripts = player.GetComponents<MonoBehaviour>();
+		foreach(MonoBehaviour script in scripts)
+		{
+			script.enabled = false;
+		}
+	}
+}
